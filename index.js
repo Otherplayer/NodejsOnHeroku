be54300222e567a5421097f98647f6c35095273e
__无邪_ <lel.guo@haihangyun.com>
2017-02-23 09:25:07 +0800

    f

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var pg = require('pg');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -17,6 +18,7 @@ app.get('/', function(request, response) {
 app.get('/cool', function(request, response) {
     response.send(cool());
 });
+
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
