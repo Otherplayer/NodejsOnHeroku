c1f15958aa1046c9bbfef0db78fe93810bb55b10
__无邪_ <fqah@qq.com>
2017-03-08 16:27:46 +0800

    f

--- index.js
+++ index.js
@@ -86,7 +86,8 @@ app.post('/gfbquestion', jsonParser, function(request, response) {
     var ans = questions[errcode];
     var speech = ans;
     if (!ans || ans == 'undefined'){
-        speech = '不好意思，未能查询到' + errcode + '的答案。请与我们的技术工程师联系。输入技术支持，查看联系方式。';
+        return request.sendStatus(400);
+        speech = '不好意思，未能查询到错误码为' + errcode + '的答案。请与我们的技术工程师联系。输入技术支持，查看联系方式。';
     }
     logger(speech);
 
